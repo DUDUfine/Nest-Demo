00ce73277648ec566beaeff050259d2446755724
DUDUfine <1607778925@qq.com>
2020-08-19 18:30:35 +0800

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Nest Demo Api')
+    .setDescription('The Api for the Nest Demo')
+    .setVersion('1.0')
+    .addTag('Api')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap().then(() => {
-  console.info(`NodePress Run！port at 3000`)
+  console.info(`NodePress Run！port at 3000`);
 });
